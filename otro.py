fa125ede42698528478d925aae9679904c3808d9
Ian Velazquez <ianvelazquez@Ians-MacBook-Air.local>
2018-04-05 17:33:20 -0500

    funcionando por primera vez

---
+++ otro.py
@@ -0,0 +1,30 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+import twitter_credentials
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["donal trump", "hillary clinton", "barack obama", "bernie sanders"]
+    fetched_tweets_filename = "tweets.txt"
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contain the user credentials to access Twitter API
+
+ACCESS_TOKEN = "312943880-fDpOZExAlQ2qtmB23cg5cPgFFJ3YiW4r8p3F8mA9"
+ACCESS_TOKEN_SECRET = "DTgMxwryEnzTySypnTRPordC4uTF7hJnI4DiikEz6m0OH"
+CONSUMER_KEY = "YCTeNQ38fQP5FZVVumidMImbq"
+CONSUMER_SECRET = "1DRXOov3fnO71jLhdqkLpiuUpqLe8JUNSWlI7s4ofbrzSX9xLy"
